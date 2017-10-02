d1ef1c40c417ce25b8ae066ed5c849685ad586e1
forxtu <forxtu@gmail.com>
2017-10-02 21:48:09 +0200

    chore(app): remove vue logo file, delete styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
     <ul>
       <router-link to="/" tag="li">Home</router-link>
       <router-link to="about" tag="li">About</router-link>
@@ -18,11 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
