d71cf32818de417f3900ddb165b055591af01e83
forxtu <forxtu@gmail.com>
2017-10-04 17:02:52 +0200

    refactor(home): remove useless styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,21 +16,7 @@ export default {
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+h1 {
+  color: purple;
 }
 </style>
