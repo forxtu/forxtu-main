bc56ab2dced3589dbd776c5109d1b7b87f71914d
forxtu <forxtu@gmail.com>
2017-10-14 22:08:04 +0200

    style(main): format

--- src/main.js
+++ src/main.js
@@ -11,7 +11,9 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
 
 /* Parallax js */
@@ -23,26 +25,25 @@ var parallaxInstance = new Parallax(appParallax, {
 });
 
 /* stars bg */
-var cols = ['#FF1053','#cecece','#ffffff','#ececec','#ecf0f1','#a2ded0'];
+var cols = ['#FF1053', '#cecece', '#ffffff', '#ececec', '#ecf0f1', '#a2ded0'];
 var stars = 100;
 
 for (var i = 0; i <= stars; i++) {
 
-	var size = Math.random()*3;
-  var color = cols[parseInt(Math.random()*4)];
+  var size = Math.random() * 3;
+  var color = cols[parseInt(Math.random() * 4)];
 
-	$('.starsBox').prepend('<span style=" width: ' + size + 'px; height: ' + size + 'px; top: ' + Math.random()*100 + '%; left: ' + Math.random()*100 + '%; background: ' + color + '; box-shadow: 0 0 '+ Math.random()*10 +'px' + color + ';"></span>') ;
+  $('.starsBox').prepend('<span style=" width: ' + size + 'px; height: ' + size + 'px; top: ' + Math.random() * 100 + '%; left: ' + Math.random() * 100 + '%; background: ' + color + '; box-shadow: 0 0 ' + Math.random() * 10 + 'px' + color + ';"></span>');
 };
 
-setTimeout(function(){ 
-	$('.starsBox span').each(function(){  
- 		$(this).css('top', Math.random()*100 + '%').css('left', Math.random()*100 + '%'); 
+setTimeout(function () {
+  $('.starsBox span').each(function () {
+    $(this).css('top', Math.random() * 100 + '%').css('left', Math.random() * 100 + '%');
   });
 }, 1);
 
-setInterval(function(){ 
-	$('.starsBox span').each(function(){  	
- 		$(this).css('top', Math.random()*100 + '%').css('left', Math.random()*100 + '%'); 
+setInterval(function () {
+  $('.starsBox span').each(function () {
+    $(this).css('top', Math.random() * 100 + '%').css('left', Math.random() * 100 + '%');
   });
-}, 100000); 
-
+}, 100000);
