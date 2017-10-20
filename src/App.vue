3bc4320e45d029767d9721fcd2f4ba5175071638
forxtu <forxtu@gmail.com>
2017-10-20 16:51:26 +0200

    refactor(app): remove 'scene element'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    
-
-    <div id="scene">
-      <p data-depth="0.2">My first Layer!</p>
-    </div>
 
     <div class="starsBox" data-depth="0.2" data-tilt></div>
     <div class="starsBox" data-depth="0.5" data-tilt style="transform-style: preserve-3d" data-tilt-perspective="4000" data-tilt-speed="100"></div>
